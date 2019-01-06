423ea261adc355990fadd7c85483f434bda25e8e
parthn <parth8nemana@gmail.com>
2019-01-06 03:49:53 +0000

    Divide Function

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 def add(x,y):passdef subtract(x,y):passdef multiply(x,y):passdef divide(x,y):
 passdef square(x,y):pass
 def multiply(x,y):return x*y
-def divide(x,y):return x/y
+def divide(x,y):
+    return x/y
