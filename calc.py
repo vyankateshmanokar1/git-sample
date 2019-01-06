129826aaa5b6048049170bb827ab37a5e81621f5
parthn <parth8nemana@gmail.com>
2019-01-06 04:17:23 +0000

    reverting divide ffunction

--- calc.py
+++ calc.py
@@ -1,5 +1,4 @@
 def add(x,y):passdef subtract(x,y):passdef multiply(x,y):passdef divide(x,y):
 passdef square(x,y):pass
 def multiply(x,y):return x*y
-def divide(x,y):
-    return x/y
+def divide(x,y): return x/y
