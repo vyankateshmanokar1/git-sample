87a8b25853434990ddfbc05c20b8f095d1524632
Parth Nemana <parth.nemana@ethans.com>
2019-07-07 05:28:08 +0000

    Changed multiply function

--- calc.py
+++ calc.py
@@ -2,10 +2,10 @@
 def add(x,y):
     pass
 def subtract(x,y):
-    return x-y
+	pass
 def multiply(x,y):
-    return x*y
+	return x*y
 def divide(x,y):
-    return x/y
+	pass
 def square(x,y):
-    pass
+	pass
